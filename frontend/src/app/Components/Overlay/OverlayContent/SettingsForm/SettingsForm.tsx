0081d74b091314a21cf976b22584c19f99fa79d5
Kirshach <kirshach@mail.ru>
2020-11-26 10:38:59 +0300

    typo fixed

--- frontend/src/app/Components/Overlay/OverlayContent/SettingsForm/SettingsForm.tsx
+++ frontend/src/app/Components/Overlay/OverlayContent/SettingsForm/SettingsForm.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Login from '../../../Login';
 import { useSelector } from 'react-redux';
-import Registration from '../../../Registration/index.tsx';
+import Registration from '../../../Registration/index';
 import { State } from '../../../../store/states/types';
 import './SettingsForm.scss';
 
